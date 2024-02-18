ae53fb6fd73c2b5b367326321261e58add51c7f7
bartosz.lagoda <composit095@gmail.com>
2024-02-18 13:57:20 +0100

    fix(FRAAT-0165): Reformat GetPostsTests

--- src/test/java/GetPostsTests.java
+++ src/test/java/GetPostsTests.java
@@ -3,14 +3,14 @@ import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.when;
 
 public class GetPostsTests {
-/*
-*  Given - warunek wstepny      Uzytkownik jest zalogowany
-*  When - wykonana akcja        Doda jeden produkt do koszyka
-*  Then - oczekiwany rezultat   Ilość produktów w koszyku powinna wzrosnąć o jeden
-* */
+    /*
+     *  Given - warunek wstepny      Uzytkownik jest zalogowany
+     *  When - wykonana akcja        Doda jeden produkt do koszyka
+     *  Then - oczekiwany rezultat   Ilość produktów w koszyku powinna wzrosnąć o jeden
+     * */
 
     @Test
-    public void getAllPostsAndLogBodyTest(){
+    public void getAllPostsAndLogBodyTest() {
         when()
                 .get("http://localhost:3000/posts")
                 .then()
@@ -18,25 +18,25 @@ public class GetPostsTests {
     }
 
     @Test
-    public void getAllPostsAndLogAllTest(){
+    public void getAllPostsAndLogAllTest() {
         when()
                 .get("http://localhost:3000/posts")
-        .then()
+                .then()
                 .log().all(); // zaloguj wszystkie informacje na temat requestu
     }
 
     @Test
-    public void getAllPostsAndValidateStatusCodeTest(){
+    public void getAllPostsAndValidateStatusCodeTest() {
         when()
                 .get("http://localhost:3000/posts")
-       .then()
+                .then()
                 .log()
                 .ifValidationFails()// zaloguj response tylko w razie błędu walidacji
                 .statusCode(200); // walidacja sprawdzajaca kod odpowiedzi czy jest to 200
     }
 
     @Test
-    public void getOnePostAndLogAllTest(){
+    public void getOnePostAndLogAllTest() {
         when()
                 .get("http://localhost:3000/posts/1")
                 .then()

d29562507e46f4d73b786d2f48d611da3a0efa43
bartosz.lagoda <composit095@gmail.com>
2024-02-18 20:19:22 +0100

    feat(FRAAT-0187): Other ways to save the code

---
+++ src/test/java/GetSpecificFieldValueTest.java
@@ -0,0 +1,44 @@
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class GetSpecificFieldValueTest {
+
+    @Test
+    public void checkSpecificFieldWithPathTest(){
+
+        Response response = RestAssured.get("http://localhost:3000/posts/1");
+        String author = response.path("author");
+
+        Assertions.assertEquals(author,"bartoszlagoda");
+    }
+
+    @Test
+    public void checkSpecificFieldWithPathWithoutVarAuthorTest(){
+
+        Response response = RestAssured.get("http://localhost:3000/posts/1").path("author");
+
+        Assertions.assertEquals(response,"bartoszlagoda");
+    }
+
+    @Test
+    public void checkSpecificFieldWithJsonPathTest(){
+
+        Response response = RestAssured.get("http://localhost:3000/posts/1");
+        JsonPath jsonPath = new JsonPath(response.asString());
+        String author = jsonPath.get("author");
+
+        Assertions.assertEquals(author,"bartoszlagoda");
+    }
+
+    @Test
+    public void checkSpecificFieldWithJsonPathWithoutVarJsonPathTest(){
+
+        String stringResponse = RestAssured.get("http://localhost:3000/posts/1").asString();
+        String author = JsonPath.from(stringResponse).get("author");
+
+        Assertions.assertEquals(author,"bartoszlagoda");
+    }
+}

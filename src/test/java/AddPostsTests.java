244ef7e634125d1a2f6cdbf2f04228d2daed5213
bartosz.lagoda <composit095@gmail.com>
2024-02-18 13:58:47 +0100

    feat(FRAAT-0168): New Post adding - push request body from file

--- src/test/java/AddPostsTests.java
+++ src/test/java/AddPostsTests.java
@@ -1,24 +1,41 @@
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+
 import static io.restassured.RestAssured.given;
-import static io.restassured.RestAssured.when;
 
 public class AddPostsTests {
 
     @Test
-    public void addPost(){
+    public void addPostAsStringTest() {
 
         String newPost = "{\n" +
-                "    \"id\": 1,\n" +
-                "    \"title\": \"Pierwszy post\",\n" +
-                "    \"author\": \"Bartosz\"\n" +
+                "    \"title\": \"Lekca 167: Dodanie nowego posta - zmiana content type\",\n" +
+                "    \"author\": \"bartoszlagoda\"\n" +
                 "}";
 
         given()
-                .body(newPost)
-        .when()
-                .body("http://localhost:3000/posts")
-        .then()
-                .log().all();
+                .log().all() // wypisanie logów requesta
+                .contentType(ContentType.JSON) // ustawienie jsonowego kontentu
+                .body(newPost) // ustalenie tego co bedzie wysylane
+                .when()
+                .post("http://localhost:3000/posts")// na jaki adres jest wysylka
+                .then()
+                .log().all(); // wyświetlenie logów response
+    }
+
+    @Test
+    public void addPostFromFileTest(){
+        File newPost = new File("src/test/resources/post.json");
+
+        given()
+                .log().all() // wypisanie logów requesta
+                .contentType(ContentType.JSON) // ustawienie jsonowego kontentu
+                .body(newPost) // ustalenie tego co bedzie wysylane
+                .when()
+                .post("http://localhost:3000/posts")// na jaki adres jest wysylka
+                .then()
+                .log().all(); // wyświetlenie logów response
     }
 }
